a0f072dd62f49efb968d4c77e839c14b98a96e85
Pedro Rosanes <prosanes@gmail.com>
2014-02-01 11:25:12 -0200

    First commit

---
+++ master_mind.rb
@@ -0,0 +1,3 @@
+def answer
+	false
+end
---
+++ master_mind_spec.rb
@@ -0,0 +1,11 @@
+require './master_mind'
+
+describe "master_mind" do
+  
+  context "true is true" do
+    it "asserts correctly" do
+		answer.should be_true
+    end
+  end  
+
+end
