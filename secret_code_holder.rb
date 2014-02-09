b0730deb510ae6ad9ce5b17f5b07ac66cb0e324e
Pedro Rosanes <prosanes@gmail.com>
2014-02-09 10:18:13 -0200

    SecretCodeHolder: answers one + if there is only one letter in the correct position

--- secret_code_holder.rb
+++ secret_code_holder.rb
@@ -5,9 +5,14 @@ class SecretCodeHolder
 
 	def get_clue_based_on(guess:guess)
 		if guess == @secret_code
-			"++++"
+			return "++++"
 		else
-			""
+			guess.split("").each_index do |i|
+				if guess[i] == @secret_code[i]
+					return "+"
+				end
+			end
+			return ""
 		end
 	end
 end
