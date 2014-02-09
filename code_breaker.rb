5ec20388a3e938833073d077e4ec357474f094e8
Pedro Rosanes <prosanes@gmail.com>
2014-02-09 14:40:34 -0200

    CodeBreaker, but still dumb

--- code_breaker.rb
+++ code_breaker.rb
@@ -1,10 +1,20 @@
 class CodeBreaker
-	attr_reader :last_guess
+	attr_reader :current_guess
 	attr_reader :made_correct_guess
 	alias_method :made_correct_guess?, :made_correct_guess
 
-	def initialize
-		@last_guess = "AABB"
+	def initialize(initial_guess:"AABB")
+		@current_guess = String.new(initial_guess)
 		@made_correct_guess = false
 	end
+
+	def make_next_guess(clue:nil)
+		if clue =="++++"
+			@made_correct_guess = true 
+		else 
+			@made_correct_guess = false
+			@current_guess << "A"
+		end
+	end
+
 end
---
+++ code_breaker_spec.rb
@@ -0,0 +1,31 @@
+require './code_breaker'
+
+describe CodeBreaker do
+	context "initialization" do
+		let(:cb) { CodeBreaker.new }
+
+		it "defaults first guess that enables the code to be broken in less then five guesses" do
+			expect(cb.current_guess).to eq("AABB")
+		end
+
+		it "doesnt know if the initial guess is correct" do
+			expect(cb.made_correct_guess?).to be_false
+		end
+	end
+
+	it "knows it a correct guess if got the winning clue" do
+		cb = CodeBreaker.new
+		cb.make_next_guess(clue:"++++")
+		expect(cb.made_correct_guess?).to be_true
+	end
+
+	it "tries another guess if the current_guess is not correct" do
+		last_guess = "AAAA"
+		cb = CodeBreaker.new(initial_guess:last_guess)
+		(1..1024).each do |i|
+			cb.make_next_guess(clue:"")
+			cb.current_guess.should_not eq(last_guess)
+			last_guess = String.new cb.current_guess
+		end
+	end
+end
