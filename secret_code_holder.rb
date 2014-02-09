d5bca5b88c5c915dbe4b889ec686911391bb09c6
Pedro Rosanes <prosanes@gmail.com>
2014-02-09 11:57:45 -0200

    SecretCodeHolder: refactor to revel intent

--- secret_code_holder.rb
+++ secret_code_holder.rb
@@ -5,18 +5,31 @@ class SecretCodeHolder
 
 	def get_clue_based_on(guess:guess)
 		clue = ""
-		already_inspectioned = Array.new(@secret_code.size) {
-									false
-								}
+		clue, remaining_guess_letters, already_inspectioned = 
+			answer_pluses(guess)
+		clue << answer_misuses(remaining_guess_letters, already_inspectioned)
+		return clue
+	end
+
+	private
+	def answer_pluses(guess)
+		already_inspectioned = Array.new(@secret_code.size) { false	}
 		remaining_guess_letters = guess.split("")
+		clue = ""
+
 		guess.split("").each_index do |i|
-			if guess[i] == @secret_code[i] and not already_inspectioned[i]
+			if guess[i] == @secret_code[i]
 				clue << "+"
 				already_inspectioned[i] = true
 				remaining_guess_letters.delete_at(i)
 			end
 		end
 
+		return clue, remaining_guess_letters, already_inspectioned
+	end
+
+	def answer_misuses(remaining_guess_letters, already_inspectioned)
+		clue = ""
 		remaining_guess_letters.each do |guess_char|
 			@secret_code.split("").each_index do |i|
 				if guess_char == @secret_code[i] and not already_inspectioned[i]
@@ -25,7 +38,6 @@ class SecretCodeHolder
 				end
 			end
 		end
-
 		return clue
 	end
 end
