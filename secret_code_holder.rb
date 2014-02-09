4e2fc5d087ef60d3586238fc037429b6a61d1a77
Pedro Rosanes <prosanes@gmail.com>
2014-02-09 10:07:28 -0200

    SecretCodeHolder: all or nothing clue answers

--- secret_code_holder.rb
+++ secret_code_holder.rb
@@ -1,4 +1,13 @@
 class SecretCodeHolder
 	def initialize(secret_code:nil)
+		@secret_code = secret_code
+	end
+
+	def get_clue_based_on(guess:guess)
+		if guess == @secret_code
+			"++++"
+		else
+			""
+		end
 	end
 end
--- secret_code_holder_spec.rb
+++ secret_code_holder_spec.rb
@@ -6,7 +6,26 @@ describe SecretCodeHolder do
 		sch = SecretCodeHolder.new(secret_code:"AAAA")
 	end
 
-	it "accepts a guess"
-	it "answers a clue based on the last guess"
+	it "answers a winning clue if the guess is correct" do
+		sch = SecretCodeHolder.new(secret_code:"AAAA")
+		clue = sch.get_clue_based_on(guess: "AAAA")
+		expect(clue).to eq("++++")
+	end
+
+	it "answers an empty clue if the guess is completely wrong" do
+		sch = SecretCodeHolder.new(secret_code:"AAAA")
+
+		clue = sch.get_clue_based_on(guess: "BBBB")
+		expect(clue).to eq("")
+
+		clue = sch.get_clue_based_on(guess: "CCCC")
+		expect(clue).to eq("")
+
+		clue = sch.get_clue_based_on(guess: "DDDD")
+		expect(clue).to eq("")
+	end
+
+	it "answers a '+' if there is one letter in the correct position" do
+	end
 
 end
