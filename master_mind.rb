7ba5e2cb5387cdc0d113e858e0b29f7f55a63198
Pedro Rosanes <prosanes@gmail.com>
2014-02-09 09:48:32 -0200

    Secret code holder class with no functionality

--- master_mind.rb
+++
@@ -1,3 +0,0 @@
-def answer
-	false
-end
---
+++ secret_code_holder_spec.rb
@@ -0,0 +1,9 @@
+require './secret_code_holder'
+
+describe SecretCodeHolder do
+  
+	it "has a secret code"
+	it "accepts a guess"
+	it "answers a clue based on the last guess"
+
+end
