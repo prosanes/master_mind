36f71fd1fc7c242e7d9eecd4609b18eb5e7491e0
Pedro Rosanes <prosanes@gmail.com>
2014-02-09 11:02:02 -0200

    SecretCodeHolder: refactored get_clue_based_on

--- secret_code_holder.rb
+++ secret_code_holder.rb
@@ -1,13 +1,17 @@
 class SecretCodeHolder
 	def initialize(secret_code:nil)
-		@secret_code = secret_code
+		@secret_code = String.new secret_code
 	end
 
 	def get_clue_based_on(guess:guess)
 		clue = ""
+		already_inspectioned = Array.new(@secret_code.size) {
+									false
+								}
 		guess.split("").each_index do |i|
-			if guess[i] == @secret_code[i]
+			if guess[i] == @secret_code[i] and not already_inspectioned[i]
 				clue << "+"
+				already_inspectioned[i] = true
 			end
 		end
 		return clue
