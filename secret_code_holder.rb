0dd22b506ae690a49910a97a22e8624ce56bc751
Pedro Rosanes <prosanes@gmail.com>
2014-02-13 20:00:31 -0200

    Removed from secret_code_holder all code responsabilities

--- secret_code_holder.rb
+++ secret_code_holder.rb
@@ -1,44 +1,12 @@
+require_relative './code'
+
 class SecretCodeHolder
 	def initialize(secret_code:nil)
-		@secret_code = String.new secret_code
+		@secret_code = secret_code
 	end
 
 	def get_clue_based_on(guess:guess)
-		clue = ""
-		clue, remaining_guess_letters, already_inspectioned = 
-			answer_pluses(guess)
-		clue << answer_misuses(remaining_guess_letters, already_inspectioned)
-		return clue
+		@secret_code.give_score_compared_to(guess)
 	end
 
-	private
-	def answer_pluses(guess)
-		already_inspectioned = Array.new(@secret_code.size) { false	}
-		remaining_guess_letters = guess.split("")
-		clue = ""
-
-		guess.split("").each_index do |i|
-			if guess[i] == @secret_code[i]
-				clue << "+"
-				already_inspectioned[i] = true
-				remaining_guess_letters.delete_at(i)
-			end
-		end
-
-		return clue, remaining_guess_letters, already_inspectioned
-	end
-
-	def answer_misuses(remaining_guess_letters, already_inspectioned)
-		clue = ""
-		remaining_guess_letters.each do |guess_char|
-			@secret_code.split("").each_index do |i|
-				if guess_char == @secret_code[i] and not already_inspectioned[i]
-					clue << "-"
-					already_inspectioned[i] = true
-					break
-				end
-			end
-		end
-		return clue
-	end
 end
--- secret_code_holder_spec.rb
+++ secret_code_holder_spec.rb
@@ -1,126 +1,14 @@
 require './secret_code_holder'
 
 describe SecretCodeHolder do
-  
-	it "has a secret code" do
-		sch = SecretCodeHolder.new(secret_code:"AAAA")
-	end
 
-	it "answers a winning clue if the guess is correct" do
-		sch = SecretCodeHolder.new(secret_code:"AAAA")
-		clue = sch.get_clue_based_on(guess: "AAAA")
+	it "gives clues based on guesses" do
+		code = double(:give_score_compared_to => "++++")
+
+		sch = SecretCodeHolder.new(secret_code:code)
+		clue = sch.get_clue_based_on(guess:code)
+
 		expect(clue).to eq("++++")
-	end
-
-	it "answers an empty clue if the guess is completely wrong" do
-		sch = SecretCodeHolder.new(secret_code:"AAAA")
-
-		clue = sch.get_clue_based_on(guess: "BBBB")
-		expect(clue).to eq("")
-
-		clue = sch.get_clue_based_on(guess: "CCCC")
-		expect(clue).to eq("")
-
-		clue = sch.get_clue_based_on(guess: "DDDD")
-		expect(clue).to eq("")
-	end
-
-	context "answers '+'s correctely" do
-		let(:sch) { SecretCodeHolder.new(secret_code: "ABCD") }
-
-		it "answers 1 '+' if there is only 1 letter in the correct position" do
-			clue = sch.get_clue_based_on(guess: "A___")
-			expect(clue.scan("+").count).to eq(1)
-
-			clue = sch.get_clue_based_on(guess: "_B__")
-			expect(clue.scan("+").count).to eq(1)
-		end
-
-		it "answers 2 '+' if there are only 2 letters in the correct position" do
-			clue = sch.get_clue_based_on(guess: "AB__")
-			expect(clue.scan("+").count).to eq(2)
-
-			clue = sch.get_clue_based_on(guess: "_BC_")
-			expect(clue.scan("+").count).to eq(2)
-
-			clue = sch.get_clue_based_on(guess: "A__D")
-			expect(clue.scan("+").count).to eq(2)
-		end
-
-		it "answers 3 '+' if there are only 3 letters in the correct position" do
-			clue = sch.get_clue_based_on(guess: "ABC_")
-			expect(clue.scan("+").count).to eq(3)
-
-			clue = sch.get_clue_based_on(guess: "_BCD")
-			expect(clue.scan("+").count).to eq(3)
-
-			clue = sch.get_clue_based_on(guess: "A_CD")
-			expect(clue.scan("+").count).to eq(3)
-		end
-	end
-
-	context "answers '-' correctly" do
-		let(:sch) { SecretCodeHolder.new(secret_code: "ABCD") }
-
-		it "answers 1 '-' if there is only one letter in the code but in the wrong position" do
-			clue = sch.get_clue_based_on(guess: "_A__")
-			expect(clue.scan("-").count).to eq(1)
-		end
-
-		it "answers 2 '-' if there are 2 letters in the code but in the wrong possition" do
-			clue = sch.get_clue_based_on(guess: "_AB_")
-			expect(clue.scan("-").count).to eq(2)
-
-			clue = sch.get_clue_based_on(guess: "_A_B")
-			expect(clue.scan("-").count).to eq(2)
-		end
-
-		it "answers 3 '-' if there are 3 letters in the code but in the wrong possition" do
-			clue = sch.get_clue_based_on(guess: "_ABC")
-			expect(clue.scan("-").count).to eq(3)
-		end
-
-		it "answers 4 '-' if there are 4 letters in the code but in the wrong possition" do
-			clue = sch.get_clue_based_on(guess: "DABC")
-			expect(clue.scan("-").count).to eq(4)
-		end
-
-		it "doesnt answer 2 '-' if there is two letters in the guess but only one in the secret code on the incorrect possition" do
-			clue = sch.get_clue_based_on(guess: "_AAA")
-			expect(clue.scan("-").count).to eq(1)
-		end
-	end
-
-	context "answer '+'s and '-'s correctly" do
-		it "code is ABCD guess is ACDB" do
-			sch = SecretCodeHolder.new(secret_code: "ABCD")
-			clue = sch.get_clue_based_on(guess: "ACDB")
-			expect(clue).to eq("+---")
-		end
-
-		it "code is ABCD guess is AAAA" do
-			sch = SecretCodeHolder.new(secret_code: "ABCD")
-			clue = sch.get_clue_based_on(guess: "AAAA")
-			expect(clue).to eq("+")
-		end
-
-		it "code is ABCD guess is BBBB" do
-			sch = SecretCodeHolder.new(secret_code: "ABCD")
-			clue = sch.get_clue_based_on(guess: "BBBB")
-			expect(clue).to eq("+")
-		end
-
-		it "code is AABB guess is BCDE" do
-			sch = SecretCodeHolder.new(secret_code: "AABB")
-			clue = sch.get_clue_based_on(guess: "BCDE")
-			expect(clue).to eq("-")
-		end
-
-		it "code is AABB guess is BBAC" do
-			sch = SecretCodeHolder.new(secret_code: "AABB")
-			clue = sch.get_clue_based_on(guess: "BBAC")
-			expect(clue).to eq("---")
-		end
 	end
 
 end
