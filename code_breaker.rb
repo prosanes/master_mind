a7f8acea96d686c227236027a5c617d25413f28e
Pedro Rosanes <prosanes@gmail.com>
2014-02-13 20:20:30 -0200

    Refactored CodeBreaker interface to comply to command & query separation

--- code_breaker.rb
+++ code_breaker.rb
@@ -1,20 +1,12 @@
 class CodeBreaker
 	attr_reader :current_guess
-	attr_reader :made_correct_guess
-	alias_method :made_correct_guess?, :made_correct_guess
 
 	def initialize(initial_guess:nil)
 		@current_guess = DeepClone.clone(initial_guess)
-		@made_correct_guess = false
 	end
 
-	def make_next_guess(clue:nil)
-		if clue =="++++"
-			@made_correct_guess = true 
-		else 
-			@made_correct_guess = false
-			@current_guess.next!
-		end
+	def receive_clue_for_current_guess(guess)
+		@current_guess.next!
 	end
 
 end
--- code_breaker_spec.rb
+++ code_breaker_spec.rb
@@ -1,27 +1,28 @@
 require './code_breaker'
 
 describe CodeBreaker do
-	context "initialization" do
-		let(:cb) { CodeBreaker.new }
-
-		it "doesnt know if the initial guess is correct" do
-			expect(cb.made_correct_guess?).to be_false
-		end
+	it "inializes with a initial_guess" do
+		cb = CodeBreaker.new(initial_guess:"AABB")
 	end
 
-	it "knows it a correct guess if got the winning clue" do
-		cb = CodeBreaker.new
-		cb.make_next_guess(clue:"++++")
-		expect(cb.made_correct_guess?).to be_true
+	it "has a current guess" do
+		cb = CodeBreaker.new(initial_guess:"AABB")
+		expect(cb.current_guess).to eq("AABB")
 	end
 
-	it "tries another guess if the current_guess is not correct" do
+	it "accepts a clue for the current guess" do
+		cb = CodeBreaker.new(initial_guess:"AABB")
+		cb.receive_clue_for_current_guess("+++-")
+	end
+
+	it "makes a new guess based on informations given" do
 		last_guess = "AAAA"
 		cb = CodeBreaker.new(initial_guess:last_guess)
 		(1..1024).each do |i|
-			cb.make_next_guess(clue:"")
+			cb.receive_clue_for_current_guess(clue:"")
 			cb.current_guess.should_not eq(last_guess)
 			last_guess = String.new cb.current_guess
 		end
 	end
+
 end
