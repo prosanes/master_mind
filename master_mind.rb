7b1fc155a2c8c69fbe5fb4d293bbc8b57bcc1381
Pedro Rosanes <prosanes@gmail.com>
2014-04-06 15:14:29 -0300

    Defined a main function and prepared for code breaker (as player) implementation

--- master_mind.rb
+++ master_mind.rb
@@ -1,14 +1,48 @@
 Dir["./domain/*.rb"].each { |f| require f }
 
-puts "Welcome to mastermind"
-puts "Do you want to play as the secret code holder or as the code breaker?"
-begin
-	puts "Please input (h) for holder or (b) for breaker."
-	p = gets.chomp
-end until p == 'h' or p == 'b'
+def main
+	puts "Welcome to mastermind"
+	puts "Do you want to play as the secret code holder or as the code breaker?"
+	begin
+		puts "Please input (h) for holder or (b) for breaker."
+		p = gets.chomp
+	end until p == 'h' or p == 'b'
 
-if p == 'h'
-	codeBreaker = CodeBreaker.new(initial_guess:CodeLetters.new("AABB"))
+	if p == 'h'
+		codeBreaker = CodeBreaker.new(initial_guess:CodeLetters.new("AABB"))
+		code = get_code_from_stdin
+
+		score = Score.new
+		while true do
+			puts "The computer guess is #{codeBreaker.current_guess.to_s}"
+			puts "Please give it the score."
+			puts "+ is for a letter in the write place."
+			puts "- is for a letter in the code, but in the wrong place." 
+			score_input = gets.chomp
+			score = ScorePlusMinus.new(score_input)
+
+			if score.correct_position != Code::NUM_PEGS
+				codeBreaker.receive_clue_for_current_guess(clue:score)
+			else
+				break
+			end
+		end
+
+		puts "The code was found: #{codeBreaker.current_guess.to_s}"
+	elsif p == 'b'
+		holder = SecretCodeHolder.new(secret_code:CodeLetters.new("AABB"))
+		while true do
+			puts "Waiting for your guess..."
+			code = get_code_from_stdin
+
+			clue = holder.get_clue_based_on(guess:code)
+			puts "The clue is #{clue.to_s}"
+		end
+	end
+
+end
+
+def get_code_from_stdin
 	begin
 		puts "Please input a four letter code between A..F"
 		code = gets.chomp
@@ -17,24 +51,6 @@ if p == 'h'
 		puts "Your code is invalid. Please try again."
 		retry
 	end
-
-	score = Score.new
-	while true do
-		puts "The computer guess is #{codeBreaker.current_guess.to_s}"
-		puts "Please give it the score."
-		puts "+ is for a letter in the write place."
-		puts "- is for a letter in the code, but in the wrong place." 
-		score_input = gets.chomp
-		score = ScorePlusMinus.new(score_input)
-
-		if score.correct_position != Code::NUM_PEGS
-			codeBreaker.receive_clue_for_current_guess(clue:score)
-		else
-			break
-		end
-	end
-
-	puts "The code was found: #{codeBreaker.current_guess.to_s}"
-elsif p == 'b'
-	computer = SecretCodeHolder.new(secret_code:CodeLetters.new("AABB"))
 end
+
+main()
